06386a62cd3b4f4c5bdf5e34d899d2fc85d0a586
victor <victor@LAYM0N>
2023-04-18 23:54:39 +0300

    Completed task 6 v 6.0

--- bot/src/main/java/ru/tinkoff/edu/java/bot/contollers/MyController.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/contollers/MyController.java
@@ -4,15 +4,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.tinkoff.edu.java.bot.dto.response.LinkUpdateResponse;
-
-import java.security.InvalidParameterException;
+import ru.tinkoff.edu.java.bot.telegrambotimpl.LinkUpdateCheckerBot;
 
 @RestController
-public class MyContoller {
-
+public class MyController {
+    private LinkUpdateCheckerBot myBot;
     @PostMapping("/updates")
     public void updateLink(@RequestBody() LinkUpdateResponse request){
-        throw new InvalidParameterException("Invalid request parameters");
-
+        myBot.sendUpdateMessages(request);
     }
 }
