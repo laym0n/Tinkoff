cd005689d20a5d109cbcf6daaf997d30a238ddd2
victor <victor@LAYM0N>
2023-05-06 03:16:16 +0300

    Update files

--- bot/src/test/java/ListBuilderSendMessageTest.java
+++ bot/src/test/java/ListBuilderSendMessageTest.java
@@ -6,25 +6,27 @@ import ru.tinkoff.edu.java.bot.telegrambotimpl.Command;
 import ru.tinkoff.edu.java.bot.telegrambotimpl.ParsedCommand;
 import ru.tinkoff.edu.java.bot.telegrambotimpl.builder.send.message.impl.buildersforeachcommand.ListBuilderSendMessage;
 import ru.tinkoff.edu.java.bot.webclients.ScrapperClient;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ListBuilderSendMessageTest {
     @Test
-    void testZeroLinksTracked(){
+    void testZeroLinksTracked() {
         //Assign
         final long chatId = 1;
-
 
         ParsedCommand commandForArgument = new ParsedCommand(chatId, Command.LIST, "");
 
         SendMessage expectedMessage =
-                new SendMessage(Long.valueOf(chatId).toString(),
-                        "Ни одной ссылки ещё не отслеживается");
+            new SendMessage(
+                Long.valueOf(chatId).toString(),
+                "Ни одной ссылки ещё не отслеживается"
+            );
 
         ListLinksResponse listLinksResponseForMock = new ListLinksResponse(
-                0, new LinkResponse[0], "", "", "", "", new String[0]
+            0, new LinkResponse[0], "", "", "", "", new String[0]
         );
         ScrapperClient scrapperClient = mock(ScrapperClient.class);
         when(scrapperClient.allLinksFromChat(chatId)).thenReturn(listLinksResponseForMock);
@@ -36,29 +38,35 @@ class ListBuilderSendMessageTest {
 
         //Assert
         assertEquals(expectedMessage.getText(), resultFromSUT.getText(),
-                ()->"Expected message is " + expectedMessage.getText() + " but result message is " + resultFromSUT.getText());
+            () -> "Expected message is " + expectedMessage.getText() + " but result message is " +
+                resultFromSUT.getText()
+        );
         assertEquals(expectedMessage.getChatId(), resultFromSUT.getChatId(),
-                ()->"Expected chatId is " + expectedMessage.getChatId() + " but result chatId is " + resultFromSUT.getChatId());
+            () -> "Expected chatId is " + expectedMessage.getChatId() + " but result chatId is " +
+                resultFromSUT.getChatId()
+        );
     }
+
     @Test
-    void testNotZeroLinksTracked(){
+    void testNotZeroLinksTracked() {
         //Assign
         final long chatId = 1;
 
         ParsedCommand commandForArgument = new ParsedCommand(chatId, Command.LIST, "");
 
-
         LinkResponse[] arrayOfLinkResponse = new LinkResponse[1];
-        arrayOfLinkResponse[0] =  new LinkResponse(1, null, "", "", "", "", new String[0]);
+        arrayOfLinkResponse[0] = new LinkResponse(1, null, "", "", "", "", new String[0]);
         ListLinksResponse listLinksResponseForMock = new ListLinksResponse(
-                1, arrayOfLinkResponse, "", "", "", "", new String[0]
+            1, arrayOfLinkResponse, "", "", "", "", new String[0]
         );
         ScrapperClient scrapperClient = mock(ScrapperClient.class);
         when(scrapperClient.allLinksFromChat(chatId)).thenReturn(listLinksResponseForMock);
 
         SendMessage notExpectedMessage =
-                new SendMessage(Long.valueOf(chatId).toString(),
-                        "Ни одной ссылки ещё не отслеживается");
+            new SendMessage(
+                Long.valueOf(chatId).toString(),
+                "Ни одной ссылки ещё не отслеживается"
+            );
 
         ListBuilderSendMessage sut = new ListBuilderSendMessage(scrapperClient);
 
@@ -67,8 +75,11 @@ class ListBuilderSendMessageTest {
 
         //Assert
         assertNotEquals(notExpectedMessage.getText(), resultFromSUT.getText(),
-                ()->"Expected message must not be " + notExpectedMessage.getText());
+            () -> "Expected message must not be " + notExpectedMessage.getText()
+        );
         assertEquals(notExpectedMessage.getChatId(), resultFromSUT.getChatId(),
-                ()->"Expected chatId is " + notExpectedMessage.getChatId() + " but result chatId is " + resultFromSUT.getChatId());
+            () -> "Expected chatId is " + notExpectedMessage.getChatId() + " but result chatId is " +
+                resultFromSUT.getChatId()
+        );
     }
 }
