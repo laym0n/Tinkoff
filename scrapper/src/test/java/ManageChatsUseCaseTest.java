fd749afb7a950155ccb9ced3bfcf65dbc6ed5019
victor <victor@LAYM0N>
2023-05-15 14:42:14 +0300

    Completed Task 10 v2.0

--- scrapper/src/test/java/ManageChatsUseCaseTest.java
+++ scrapper/src/test/java/ManageChatsUseCaseTest.java
@@ -5,13 +5,14 @@ import ru.tinkoff.edu.java.scrapper.dataaccess.ChatDAService;
 import ru.tinkoff.edu.java.scrapper.entities.Chat;
 import ru.tinkoff.edu.java.scrapper.usecases.ManageChatsUseCase;
 import ru.tinkoff.edu.java.scrapper.usecases.impl.managechats.ManageChatsUseCaseImpl;
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ManageChatsUseCaseTest {
     @Test
-    void testValidRegistryChat(){
+    void testValidRegistryChat() {
         //Assign
         ChatDAService chatDAService = mock(ChatDAService.class);
         ManageChatsUseCase sut = new ManageChatsUseCaseImpl(chatDAService);
@@ -22,8 +23,9 @@ class ManageChatsUseCaseTest {
         //Assert
         verify(chatDAService).createChat(new Chat(1));
     }
+
     @Test
-    void testValidRemoveChat(){
+    void testValidRemoveChat() {
         //Assign
         ChatDAService chatDAService = mock(ChatDAService.class);
         ManageChatsUseCase sut = new ManageChatsUseCaseImpl(chatDAService);
